e25c89da9dab70861048d5d9410ba88744cfe564
Kaustav Vats <kaustav16048@iiitd.ac.in>
2018-05-21 02:30:13 +0530

    Not Completed

---
+++ app/src/main/java/com/chatapp/kvats/chattingapp/FriendlyMessage.java
@@ -0,0 +1,50 @@
+package com.chatapp.kvats.chattingapp;
+
+import android.nfc.Tag;
+import android.util.Log;
+
+public class FriendlyMessage {
+
+    private String Tag = "FriendlyMessage";
+    private String text;
+    private String name;
+    private String photoUrl;
+
+    public FriendlyMessage() {
+    }
+
+    public FriendlyMessage(String text, String name, String photoUrl) {
+        this.text = text;
+        this.name = name;
+        this.photoUrl = photoUrl;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public void print()
+    {
+        Log.d(Tag, text+"\t"+name+"\n");
+    }
+}
